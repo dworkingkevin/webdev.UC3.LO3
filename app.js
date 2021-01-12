b9a1a55d5ae1f0060988e5b37c4bab6e343c677b
dworkingkevin <dworkingkevin@gmail.com>
2021-01-12 17:02:42 +0800

    third

---
+++ app.js
@@ -0,0 +1,56 @@
+//events
+
+const btns = document.querySelectorAll("#book-list .delete");
+
+Array.from(btns).forEach(function(btn){btn.addEventListener("click", function(e){
+
+
+const li = e.target.parentElement;
+li.parentNode.removeChild(li)
+
+ });
+});
+
+const link = documents.querySelector("#page-banner a");
+
+link.addEventListener("click", function(e){
+e.preventDefault();
+console.log("navigation to ", e.target.textContent," was prevented");
+});
+
+// Event Bubbling
+
+const list = document.querySelector("#book-list ul");
+
+//delete books
+list.addEventListener("click", function(e){
+if(e.target.className == "delete"){
+ const li = e.target.parentElement;
+ list.removeChild(li);
+ }
+});
+
+// adding book-list
+
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", function(e){
+ e.preventDefault();
+ const value = addForm.querySelector('input[type="text"]').value;
+ console.log(value);
+});
+
+//Creating Elements
+
+const li = document.createElement("li");
+const bookName = document.createElement("span");
+const deleteBtn = document.createElement("span");
+
+//adding content
+deleteBtn.textContent ="delete";
+bookName.textContent = value
+
+//append to document
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
